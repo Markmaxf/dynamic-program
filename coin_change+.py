d1b326784b244fc3cabdc1e4b0191bd10c69f202
unknown <xiaofei.ma@aggiemail.usu.edu>
2018-05-23 20:41:15 -0600

    modified coin_change

--- coin_change+.py
+++ coin_change+.py
@@ -1,24 +1,43 @@
 
 # coding: utf-8
 
-# In[3]:
+# In[14]:
 
 
-total=int(input())
-coins=list(map(int,input().split()))
+
+nm = input().split()
+
+n = int(nm[0])
+
+m = int(nm[1])
+
+c = list(map(int, input().split()))
+c.sort()
 def getWays(n, c):
     if len(c)==1:
-        return 1
+        return (1 if n%c[0]==0 else 0)
     elif len(c)>1:
         if n<c[-1]:
-            return 1
+            return getWays(n,c[0:-1])
         else:
             return getWays(n,c[0:-1])+getWays(n-c[-1],c)
 
 
         
-ways=getWays(total,coins)
+ways=getWays(n,c)
 print(ways)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
 
 
 # In[ ]:
